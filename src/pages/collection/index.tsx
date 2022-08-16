d6155263a10b2df370073ce158b49e5d04423a21
goburhan <goburhann@gmail.com>
2022-08-16 17:58:23 +0300

    info deleted

--- src/pages/collection/index.tsx
+++ src/pages/collection/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import SectionSelectButtons from "../../components/SectionSelectButtons";
 import CollectionsHeader from "./CollectionsHeader";
-import InfoSection from "../carddetail/Components/InfoSection";
 
 
 export default function Collection() {
